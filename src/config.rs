89f795854e1f2eb16dff29cf8740e78ab0eb61cf
Aleks Rutins <aleks@rutins.com>
2022-02-20 17:54:00 -0500

    Use a subdirectory

--- src/config.rs
+++ src/config.rs
@@ -15,22 +15,25 @@ pub struct Recipe {
 }
 
 lazy_static! {
-    pub static ref CONFIG_DIR: String = dirs::config_dir().expect("Could not get config dir").to_str().expect("Could not convert path to string").to_string() + "/autobuild/recipes";
+    pub static ref CONFIG_DIR: String = dirs::config_dir().expect("Could not get config dir").to_str().expect("Could not convert path to string").to_string() + "/autobuild";
 }
 
+pub fn config_dir(subdir: &str) -> String { CONFIG_DIR.to_string() + "/" + subdir }
+
 pub fn get_installed_recipes() -> HashMap<String, Vec<Recipe>> {
-    let dir = fs::read_dir(CONFIG_DIR.as_str());
+    let recipes_dir = &config_dir("recipes");
+    let dir = fs::read_dir(recipes_dir);
     match dir {
         Ok(_) => {
             let mut result: HashMap<String, Vec<Recipe>> = HashMap::new();
-            for entry in WalkDir::new(CONFIG_DIR.as_str()).follow_links(true).into_iter().filter_map(|entry| entry.ok()) {
+            for entry in WalkDir::new(recipes_dir).follow_links(true).into_iter().filter_map(|entry| entry.ok()) {
                 if entry.file_type().is_file() {
                     match File::open(entry.path()) {
                         Ok(mut file) => {
                             if let Ok(content) = read_file(&mut file) {
                                 if let Ok(recipe) = ron::from_str::<Recipe>(content.as_str()) {
                                     if result.contains_key(&recipe.provides) {
-                                        if let Some(mut entry) = result.get_mut(&recipe.provides) {
+                                        if let Some(entry) = result.get_mut(&recipe.provides) {
                                             entry.append(&mut vec![recipe]);
                                         }
                                     } else {
@@ -50,8 +53,8 @@ pub fn get_installed_recipes() -> HashMap<String, Vec<Recipe>> {
             result
         }
         Err(_) => {
-            console::log_info("No config directory found; creating");
-            fs::create_dir_all(CONFIG_DIR.as_str());
+            console::log_info(&format!("Creating configuration directory {}", recipes_dir));
+            fs::create_dir_all(recipes_dir);
             HashMap::new()
         }
     }
